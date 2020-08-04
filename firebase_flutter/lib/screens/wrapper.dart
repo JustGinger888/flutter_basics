68076e54c38275003f41c22ccea00ed24203edda
JustGinger888 <ethanmoffatt888@icloud.com>
2020-08-04 04:39:15 +0100

    signIn

--- firebase_flutter/lib/screens/wrapper.dart
+++ firebase_flutter/lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_flutter/screens/authenticate/authenticate.dart';
 import 'package:firebase_flutter/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
--- firebase_flutter/lib/services/auth.dart
+++ firebase_flutter/lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_flutter/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Create User Object based on Firebase User
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   // Sign In Anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
